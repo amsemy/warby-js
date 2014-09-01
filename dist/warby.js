ae5fd15fc3de9c5ab0ed39a3ef75aab12a599da3
Amsemy Teladi <amsemy@gmail.com>
2014-09-01 12:46:24 +0700

    Fix filename

--- dist/warby.js
+++ dist/warby.js
@@ -713,3 +713,34 @@
     }
 
 })(gumup);
+
+(function(ns) {
+
+    'use strict';
+
+    ns.unit('com.github.amsemy.warby.restyProvider', function() {
+
+        return function(model, options) {
+            var success = options.success;
+            options.success = function(resp) {
+                switch (resp.status) {
+                    case "INVALID":
+                        if (options.error) {
+                            options.error(resp.errors);
+                        }
+                        break;
+                    case "SUCCESS":
+                        if (success) {
+                            success(resp.data);
+                        }
+                        break;
+                    default:
+                        if (options.error) {
+                            options.error();
+                        }
+                }
+            };
+        };
+    });
+
+})(gumup);
